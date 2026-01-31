dd84086e6a4e0cbf8e4c06a59ca614015dc0eadb
Evgeny Kryshen <evgeny.kryshen@cern.ch>
2026-01-31 17:02:44 +0300

    Error propagation for analytic spacepoint maker

--- macros/analytical_space_point_maker.C
+++ macros/analytical_space_point_maker.C
@@ -4,6 +4,7 @@ const int n = 3;
 double a[n];
 double b[n];
 double c[n];
+double sigma = 0.1;
 
 double func(const double* xx){
   double t = xx[0];
@@ -16,21 +17,100 @@ double func(const double* xx){
   return d2;
 }
 
+void analytic(double &t, double &k, double &dt, double &dk, double& A, double& B, double& sigA2, double& sigB2, double& covAB, bool debug = 0){
+  // analytical computation
+  double s[n];
+  double aa[n]={0};
+  double bb[n]={0};
+  A = 0;
+  B = 0;
+  sigA2 = 0;
+  sigB2 = 0;
+  covAB = 0;
+
+  for (int i=0;i<n;i++){
+    s[i] = sigma;
+    aa[i] = 0;
+    bb[i] = 0;
+    for (int j=0;j<n;j++){
+      double ab = (a[i]*b[j]-b[i]*a[j]);
+      aa[i]+=b[j]*ab;
+      bb[i]+=a[j]*ab;
+    }
+    A+= c[i]*aa[i];
+    B+= c[i]*bb[i];
+    sigA2+=s[i]*s[i]*aa[i]*aa[i];
+    sigB2+=s[i]*s[i]*bb[i]*bb[i];
+    covAB+=s[i]*s[i]*aa[i]*bb[i];
+  }
+
+
+  if (debug) printf("A=%e\n",A);
+  if (debug) printf("B=%e\n",B);
+  if (debug) printf("sigA2=%e\n",sigA2);  
+  if (debug) printf("sigB2=%e\n",sigB2);  
+  if (debug) printf("covAB=%e\n",covAB);  
+  k = - B/A;
+  dk = sqrt(B*B*sigA2+A*A*sigB2-2*A*B*covAB)/A/A;
+  if (debug) printf("k=%e dk=%e dk/k=%f\n",k, dk, dk/k);  
+
+  double num = 0;
+  double den = 0;
+  double abk[n] = {0};
+  double vdt[n] = {0};
+  
+  double sigT2[n] = {0};
+  double sigC2[n] = {0};
+  double covCT[n] = {0};
+  double p1 = 0;
+  double p2 = 0;
+  for (int i=0;i<n;i++){
+    abk[i] = a[i]+b[i]*k;
+    num += c[i]*abk[i];
+    den += abk[i]*abk[i];
+    p1 += a[i]*b[i];
+    p2 += b[i]*b[i];
+  }
+  t = -sqrt(1+k*k)*num/den;
+
+  double vt[n] = {0};
+  double dtdk[n] = {0};
+  double dkdc[n] = {0};
+  for (int i=0; i<n; i++){
+    vt[i]  = -sqrt(1+k*k)*abk[i]/den;
+    dtdk[i] = vt[i]*(k/(1+k*k) + b[i]/abk[i]-2*(p1+p2*k)/den);
+    dkdc[i] = (aa[i]*B - bb[i]*A)/A/A;
+    if (debug) printf("i=%d t[i]=%e dtdk[i]*dk=%e dtdk[i]*dk/t[i]=%f\n",i, vt[i],dtdk[i]*dk,dtdk[i]*dk/vt[i]);
+  }
+  double dtdc[n] = {0};
+  double dt2 = 0;
+  for (int i=0; i<n; i++){
+    if (debug) printf("i=%d k[i]=%e dkdc[i]*dc[i]=%e dkdc[i]*dc/k[i]=%f\n",i,k,dkdc[i]*s[i],dkdc[i]*s[i]/k);
+    dtdc[i] = vt[i];
+    for (int j=0; j<n; j++){
+      dtdc[i] += c[j]*dtdk[j]*dkdc[i];
+    }
+    dt2+=s[i]*s[i]*dtdc[i]*dtdc[i];
+  }
+  dt = sqrt(dt2);
+  if (debug) printf("t=%e dt=%e dt/t=%f\n",t,dt,dt/t);
+}
+
 void analytical_space_point_maker(){
+  double alphaTrueDeg = 8;
   double thetaTrue = 30*TMath::DegToRad();
-  double alphaTrue = 14*TMath::DegToRad();
+  double alphaTrue = alphaTrueDeg*TMath::DegToRad();
   double tTrue = tan(thetaTrue);
   double kTrue = tan(alphaTrue);
   double r[] = { 100,  100,  100}; // station minimal radius
   double z[] = {2100, 2110, 2120}; // station pozition
-  double phiDeg[] = {10, 15, 20};
+  double phiDeg[] = {alphaTrueDeg-4, alphaTrueDeg+1, alphaTrueDeg+6};
   double d[n];
   double phi[n];
   double xTrue[n];
   double yTrue[n];
   double xmeas[n];
   double ymeas[n];
-  double sigma = 0.1;
   for (int i=0;i<n;i++){
     d[i] = gRandom->Gaus(0,sigma);
     phi[i] = phiDeg[i]*TMath::DegToRad();
@@ -68,27 +148,133 @@ void analytical_space_point_maker(){
   double kk = tan(aa);
 
   // analytical computation
-  double A = 0;
-  double B = 0;
-  for (int i=0;i<n;i++){
-    for (int j=0;j<n;j++){
-      double ab = (a[i]*b[j]-b[i]*a[j]);
-      A+= c[i]*b[j]*ab;
-      B+= c[i]*a[j]*ab;
-    }
-  }
-  printf("A=%f\n",A);
-  printf("B=%f\n",B);
-  double k = - B/A;
-  double num = 0;
-  double den = 0;
-  for (int i=0;i<n;i++){
-    double abk = a[i]+b[i]*k;
-    num+=c[i]*abk;
-    den+=abk*abk;
-  }
-  double t = -sqrt(1+k*k)*num/den;
+  double t,k,dt,dk,A,B,A0,B0,sigA2,sigB2,covAB;
+  analytic(t,k,dt,dk,A,B,sigA2,sigB2,covAB,1);
   
   printf("kk=%.10f k=%.10f kTrue=%f\n", kk, k, kTrue);
   printf("tt=%.10f t=%.10f tTrue=%f\n", tt, t, tTrue);
+  xx[0]=t;
+  xx[1]=atan(k);
+  double d2 = func(xx);
+  printf("Squared sum of distances: %f\n", d2);
+
+  for (int i=0;i<n;i++){
+    c[i] = -xTrue[i]*sin(phi[i]) + yTrue[i]*cos(phi[i]);
+  }
+  analytic(t,k,dt,dk,A0,B0,sigA2,sigB2,covAB,1);
+  A0*=1e-11;
+  B0*=1e-11;
+  double c0true=c[0];
+  double cTrue[n];
+  cTrue[0] = c[0];
+  cTrue[1] = c[1];
+  cTrue[2] = c[2];    
+  // double numTrue[n] = {0};
+  // double denTrue = 0;
+  // double tttTrue[n] = {0};
+  // for (int i=0;i<n;i++){
+  //   double abkTrue = a[i]+b[i]*k;
+  //   numTrue[i] = abkTrue;
+  //   denTrue+=abkTrue*abkTrue;
+  // }
+  // for (int i=0;i<n;i++){
+  //   tttTrue[i] = -sqrt(1+k*k)*numTrue[i]/denTrue;
+  // }
+
+
+  // error propagation test
+  TH1D* hT = new TH1D("hT","",150,0.57,0.585);
+  TH1D* hK = new TH1D("hK","",200,kTrue-0.0005,kTrue+0.0005);
+  TH1D* hAtanK = new TH1D("hAtanK","",200,alphaTrueDeg-0.02,alphaTrueDeg+0.02);
+  TH1D* hPullK = new TH1D("hPullK","",100,-5,5);
+  TH1D* hPullT = new TH1D("hPullT","",100,-5,5);
+  TH2D* hTK = new TH2D("kTK",";T;K;",150,0.57,0.585,150,0.2485,0.25);
+  TH2D* hAB = new TH2D("hAB",";A;B;",100,A0-0.02,A0+0.02,100,B0-0.02,B0+0.02);
+  TH2D* hCK = new TH2D("hCK",";C;K;",100,1-1e-2,1+1e-2, 200,1-1e-2,1+1e-2);
+  TH1D* hTTT0 = new TH1D("hTTT0","",200,0.00202,0.00205);
+  TH1D* hPullTTT0 = new TH1D("hPullTTT0","",100,-5,5);
+  TH2D* hPullC0_vs_TTT0 = new TH2D("hPullC0_vs_TTT0","",100,-5,5,100,-5,5);
+  TH2D* hPullC1_vs_TTT1 = new TH2D("hPullC1_vs_TTT1","",100,-5,5,100,-5,5);
+  TH2D* hPullC2_vs_TTT2 = new TH2D("hPullC2_vs_TTT2","",100,-5,5,100,-5,5);
+
+  for (int ev=0;ev<100000;ev++){
+    //if (ev%100==0) printf("%d\n",ev);
+    for (int i=0;i<n;i++){
+      d[i] = gRandom->Gaus(0,sigma);
+      xmeas[i] = xTrue[i]+d[i]*sin(phi[i]);
+      ymeas[i] = yTrue[i]-d[i]*cos(phi[i]);
+      c[i] = -xmeas[i]*sin(phi[i]) + ymeas[i]*cos(phi[i]);
+    }  
+    analytic(t,k,dt,dk,A,B,sigA2,sigB2,covAB);
+    hT->Fill(t);
+    hK->Fill(k);
+    hTK->Fill(t,k);
+    hPullK->Fill((k-kTrue)/dk);
+    hAtanK->Fill(atan(k)*TMath::RadToDeg());
+    hAB->Fill(A*1.e-11,B*1.e-11);
+    hCK->Fill(c[0]/c0true,k/kTrue);
+
+    // double num[n] = {0};
+    // double den = 0;
+    // double ttt[n] = {0};
+    // double dttt[n] = {0};
+    // double p1 = 0;
+    // double p2 = 0;
+    // for (int i=0;i<n;i++){
+    //   double abk = a[i]+b[i]*k;
+    //   num[i] = abk;
+    //   den+=abk*abk;
+    //   p1+=2*a[i]*b[i];
+    //   p2+=  b[i]*b[i];
+    // }
+    // for (int i=0;i<n;i++){
+    //   ttt[i] = -sqrt(1+k*k)*num[i]/den;
+    //   dttt[i] = ttt[i]*(k/(1+k*k) + b[i]/num[i]-(p1+2*p2*k)/den);
+    // }
+
+    // hTTT0->Fill(ttt[0]);
+    // hPullTTT0->Fill((ttt[0]-tttTrue[0])/(dttt[0]*dk));    
+    // printf("%f %e\n",ttt[0], dttt[0]*dk);
+
+    // double pullTTT0 = (ttt[0]-tttTrue[0])/(dttt[0]*dk);
+    // double pullTTT1 = (ttt[1]-tttTrue[1])/(dttt[1]*dk);
+    // double pullTTT2 = (ttt[2]-tttTrue[2])/(dttt[2]*dk);
+    // double pullC0 = (c[0]-cTrue[0])/sigma;
+    // double pullC1 = (c[1]-cTrue[1])/sigma;
+    // double pullC2 = (c[2]-cTrue[2])/sigma;    
+    // hPullC0_vs_TTT0->Fill(pullC0,pullTTT0);
+    // hPullC1_vs_TTT1->Fill(pullC1,pullTTT1);
+    // hPullC2_vs_TTT2->Fill(pullC2,pullTTT2);
+    double pullT = (t - tTrue)/dt;
+    hPullT->Fill(pullT);
+  }
+  new TCanvas;
+  hT->Draw();
+  new TCanvas;
+  hTK->Draw();
+  new TCanvas;
+  hPullK->Draw();
+  hPullK->Fit("gaus");
+  new TCanvas;
+  hAtanK->Draw();
+  hAtanK->Fit("gaus");
+  new TCanvas;
+  hAB->Draw();
+  new TCanvas;
+  hCK->Draw();
+  new TCanvas;
+  hTTT0->Draw();
+  hTTT0->Fit("gaus");
+  new TCanvas;
+  hPullTTT0->Draw();
+  hPullTTT0->Fit("gaus");
+  new TCanvas;
+  hPullC0_vs_TTT0->Draw();
+  new TCanvas;
+  hPullC1_vs_TTT1->Draw();
+  new TCanvas;
+  hPullC2_vs_TTT2->Draw();
+  new TCanvas;
+  hPullT->Draw();
+  hPullT->Fit("gaus");
 }
